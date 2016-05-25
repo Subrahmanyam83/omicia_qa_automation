3725c12a1c4ff91a369447217b7e65675246f8e2
Subrahmanyam <rvsubrahmanyam1983@gmail.com>
2016-05-25 16:55:27 -0700

    changing the windowSize in GebConfig for firefox

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -126,7 +126,7 @@ environments {
                 browserDriver.setFileDetector(new LocalFileDetector())
             }
             browserDriver.manage().timeouts().pageLoadTimeout(30, TimeUnit.MINUTES)
-            browserDriver.manage().window().setSize(new Dimension(1200, 800))
+            browserDriver.manage().window().setSize(new Dimension(1280,1024))
             browserDriver.manage().window().maximize()
             return browserDriver;
         }
